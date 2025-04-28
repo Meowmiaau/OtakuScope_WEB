fe11db21e23b38bba46e78cd7cb2cfe559690ec5
afsana shehrin <afsanashehrin@gmail.com>
2025-04-28 13:26:29 +0600

    Staff Page Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import Home from './pages/Home';
 import ListDetails from './pages/ListDetails';
 import Lists from './pages/Lists';
 import Character from './components/Character';
+import Staff from './components/Staff';
 const theme = createTheme({
     palette: {
         mode: 'dark', // Dark mode
@@ -31,6 +32,7 @@ const App = () => (
                 <Route path="/" element={<GetStarted />} />
                 <Route path="/anime/:id" element={<AnimeDetails />} />
                 <Route path="/anime/:id/character" element={<Character />} />
+                <Route path="/anime/:id/staff" element={<Staff />} />
                 <Route path="/:id" element={<AnimeDetails />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
